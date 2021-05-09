ac6b0a344fc1b4bca465f77cb08a47e71126213e
Rinat <rinamuka4@gmail.com>
2021-05-10 00:42:27 +0300

    add funny photo to reply

--- main.go
+++ main.go
@@ -2,19 +2,67 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	time2 "time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-const token = "#"
-const apiKey = "#"
+func getSeason() string {
+	month := time2.Now().Month()
+	log.Println(int(month))
+	if month >= 6 && month <= 8 {
+		return "summer"
+	} else if month > 8 && month <= 11 {
+		return "autumn"
+	} else if month > 11 && month <= 2 {
+		return "winter"
+	} else {
+		return "spring"
+	}
+}
+
+func getImageAboutWeather(temp float64) string {
+	var goodWthr string = "./images/goodWeather.png"
+	var badWthr string = "./images/badWeather.png"
+	season := getSeason()
+	if season == "summer" {
+		if temp > 20 {
+			return goodWthr
+		} else {
+			return badWthr
+		}
+	} else if season == "autumn" {
+		if temp > 5 {
+			return goodWthr
+		} else {
+			return badWthr
+		}
+	} else if season == "winter" {
+		if temp > -10 {
+			return goodWthr
+		} else {
+			return badWthr
+		}
+	} else if season == "spring" {
+		if temp > 5 {
+			return goodWthr
+		} else {
+			return badWthr
+		}
+	} else {
+		errors.New("Error when get season")
+	}
+	return ""
+}
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(token)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("botToken"))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -32,7 +80,7 @@ func main() {
 			continue
 		}
 
-		res, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + update.Message.Text + "&appid=" + apiKey)
+		res, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + update.Message.Text + "&appid=" + os.Getenv("apiToken"))
 
 		if res.StatusCode == http.StatusNotFound {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprint("Город не найден"))
@@ -46,13 +94,18 @@ func main() {
 			log.Fatal(err)
 		}
 
-		response := weather{}
+		wthr := weather{}
 		body, _ := ioutil.ReadAll(res.Body)
-		json.Unmarshal(body, &response)
-		log.Println(response.Main.Temp)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprint(int(response.Main.Temp-272), " градусов"))
-		msg.ReplyToMessageID = update.Message.MessageID
+		err = json.Unmarshal(body, &wthr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(getSeason())
+		image := getImageAboutWeather(wthr.Main.Temp - 272)
+		uploadPhoto := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, image)
+		uploadPhoto.Caption = fmt.Sprint(int(wthr.Main.Temp-272), " градусов")
 
-		bot.Send(msg)
+		log.Println(wthr.Main.Temp)
+		bot.Send(uploadPhoto)
 	}
 }
