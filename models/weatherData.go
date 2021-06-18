9cddb324fbe6b63f5591b21b9dba3a4c29621f23
Rinat <rinamuka4@gmail.com>
2021-06-19 01:54:58 +0300

    added time of day processing

--- models/weatherData.go
+++ models/weatherData.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type Weather struct {
@@ -18,28 +19,37 @@ type Weather struct {
 		TempMin   float64 `json:"temp_min"`
 		TempMax   float64 `json:"temp_max"`
 	} `json:"main"`
+	Sys struct {
+		Sunrise int `json:"sunrise"`
+		Sunset  int `json:"sunset"`
+	} `json:"sys"`
 }
 
 func (w Weather) GetImage() string {
-	var clouds = "./images/clouds.jpg"
-	var snow = "./images/snow.jpg"
-	var rain = "./images/rain.jpg"
-	var clear = "./images/clear.jpg"
-	season := getSeason()
-	fmt.Println(season)
-	if w.Weather[0].Main == "Clouds" {
-		return clouds
-	} else if w.Weather[0].Main == "Snow" {
-		return snow
-	} else if w.Weather[0].Main == "Rain" {
-		return rain
-	} else if w.Weather[0].Main == "Clear" {
-		return clear
+	image := "./images/"
+	var timeOfDay string
+	fmt.Println(time.Now().Unix())
+	if int(time.Now().Unix()) < w.Sys.Sunrise || int(time.Now().Unix()) > w.Sys.Sunset {
+		timeOfDay = "night"
 	} else {
+		timeOfDay = "day"
+	}
+
+	switch weather := w.Weather[0].Main; weather {
+	case "Clouds":
+		return image + timeOfDay + "_clouds.jpg"
+	case "Snow":
+		return image + timeOfDay + "_snow.jpg"
+	case "Rain":
+		return image + timeOfDay + "_rain.jpg"
+	case "Clear":
+		return image + timeOfDay + "_clear.jpg"
+	default:
 		log.WithFields(log.Fields{
 			"package":  "models",
 			"function": "GetImage",
 		}).Warning("Cannot get image, unknown weather.Main")
 		return ""
 	}
+
 }
