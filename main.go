2676e5bc7aa72e4707f4a595618ff88aa3bf6985
Rinat <rinamuka4@gmail.com>
2021-06-20 00:23:47 +0300

    add webhook

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-
-func Hello(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "POshel nahoy")
-}
 func main() {
-	log.Fatal(http.ListenAndServeTLS(":8080","certs/server.crt","certs/server.key", nil))
+	bot, err := tgbotapi.NewBotAPI("1796557206:AAEQFvvs2olVY6M9-zicV_YRW-FtLqTgQEM")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("185.251.91.103:8443/"+bot.Token, "certs/server.crt"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:8080", "certs/server.crt", "server.key", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
 }
