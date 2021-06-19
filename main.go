8953f47047f7cef186be58df77509d0073dfb85b
Rinat <rinamuka4@gmail.com>
2021-06-20 00:33:13 +0300

    add webhook

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("185.251.91.103:8443/"+bot.Token, "certs/server.crt"))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("185.251.91.103:8443/"+bot.Token, "certs/cert.pem"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,7 +29,7 @@ func main() {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
 	}
 	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("0.0.0.0:8080", "certs/server.crt", "server.key", nil)
+	go http.ListenAndServeTLS("0.0.0.0:8080", "certs/cert.pem", "key.pem", nil)
 
 	for update := range updates {
 		log.Printf("%+v\n", update)
