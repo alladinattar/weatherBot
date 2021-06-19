3fb9175bdce9fc8a6b23c141ef5cc5f2a2f883e9
Rinat <rinamuka4@gmail.com>
2021-06-20 00:01:42 +0300

    add webhook

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+
+func Hello(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "POshel nahoy")
+}
 func main() {
 	log.Fatal(http.ListenAndServeTLS(":8080","certs/server.crt","certs/server.key", nil))
 
